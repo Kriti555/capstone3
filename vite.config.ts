6a5270ddad073f0c633d566d57a4bc3016e169c8
Kriti555 <156103301+Kriti555@users.noreply.github.com>
2024-02-06 20:47:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Kriti555/capstone3.git"
+  base: "https://kriti555.github.io/capstone3"
 })
